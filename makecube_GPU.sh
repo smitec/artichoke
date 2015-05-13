a24281f0566f77fa388e0d7ae779d67f872f2e6f
Elliot Smith <elliot.smith91@gmail.com>
2015-05-14 09:42:25 +1000

    made the scene a little more complex

--- makecube_GPU.sh
+++ makecube_GPU.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -v ${PWD}/output:/output -v ${PWD}/cube.blend:/input/cube.blend -v ${PWD}/cycles_gpu.py:/input/cycles_gpu.py b0b57 /usr/local/blender/blender -b /input/cube.blend -o /output/result_### -E CYCLES -P /input/cycles_gpu.py -f 1
+docker run --rm -v ${PWD}/output:/output -v ${PWD}/cube.blend:/input/cube.blend -v ${PWD}/cycles_gpu.py:/input/cycles_gpu.py b0b57 /usr/local/blender/blender -b /input/cube.blend -o /output/result_### -E CYCLES -P /input/cycles_gpu.py -f 71
